f4a829a79987bb23266c608c47460ea4d564d68d
Khiem Nguyen <ng.khiem97vn@gmail.com>
2023-02-11 14:33:57 -0500

    add inception model

---
+++ image_classification/model/inception_v3.py
@@ -0,0 +1,25 @@
+import urllib
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+
+INPUT_SHAPE = (150, 150, 3)
+
+def get_inception_v3_model(weights_url, weights_file):
+    print('Downloading InceptionV3 pre-trained model...')
+    urllib.request.urlretrieve(weights_url, weights_file)
+    pre_trained_model = InceptionV3(input_shape=INPUT_SHAPE,
+                                    include_top=False,
+                                    weights=None)
+    pre_trained_model.load_weights(weights_file)
+    for layer in pre_trained_model.layers:
+        layer.trainable = False
+    last_layer = pre_trained_model.get_layer('mixed7')
+    print('last layer output shape: ', last_layer.output_shape)
+    last_output = last_layer.output
+    x = layers.Flatten()(last_output)
+    x = layers.Dense(1024, activation='relu')(x)
+    x = layers.Dense(1, activation='sigmoid')(x)
+    model = Model(pre_trained_model.input, x)
+    print('Model created successfully')
+    return model
