5491209c06c92f1c8f8c28799505ef6a271bfcbd
Khiem Nguyen <ng.khiem97vn@gmail.com>
2023-02-14 23:07:38 -0500

    add model and data preprocessing

--- image_classification/model/resnet50.py
+++ image_classification/model/resnet50.py
@@ -14,11 +14,10 @@ def classifier(inputs):
     x = tf.keras.layers.Dense(10, activation="softmax", name="classification")(x)
     return x
 
-def get_model(optimizer='SGD', loss='sparse_categorical_crossentropy', metrics=['accuracy']):
+def get_model():
   inputs = tf.keras.layers.Input(shape=(32,32,3))
   resize = tf.keras.layers.UpSampling2D(size=(7,7))(inputs)
   resnet_feature_extractor = feature_extractor(resize)
   resnet_classifier = classifier(resnet_feature_extractor)
   model = tf.keras.Model(inputs=inputs, outputs=resnet_classifier)
-  model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
   return model
--- image_classification/utils/data.py
+++ image_classification/utils/data.py
@@ -1,6 +1,26 @@
 import tensorflow as tf
+from matplotlib import pyplot as plt
+import numpy as np
 
 def preprocess_image_input(input_images):
   input_images = input_images.astype('float32')
   output_ims = tf.keras.applications.resnet50.preprocess_input(input_images)
   return output_ims
+
+def display_images(digits, predictions, title, classes):
+  n = 10
+  indexes = np.random.choice(len(predictions), size=n)
+  n_digits = digits[indexes]
+  n_predictions = predictions[indexes]
+  n_predictions = n_predictions.reshape((n,))
+  fig = plt.figure(figsize=(20, 4))
+  plt.title(title)
+  plt.yticks([])
+  plt.xticks([])
+  for i in range(10):
+    ax = fig.add_subplot(1, 10, i+1)
+    class_index = n_predictions[i]
+    plt.xlabel(classes[class_index])
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(n_digits[i])
